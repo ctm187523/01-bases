021e87dd554a3f56076bcd37f6cfd319f1c95e85
CTM <clemente1875gmail.com>
2022-10-01 09:51:14 +0200

    cambios

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -2,12 +2,12 @@ import { useState } from "react"
 
 //usamos la interface para crear las props que definen el tipo que tiene de dato que tiene que recibir
 //podemos poner el interrogante para indicar que es opcional el initialValue que seria el valor que recibe del
-//componente App.tsx y ponemos por defecto initialValue = 0 en caso de no recibir nada del App.tsx
+//componente App.tsx y ponemos por defecto initialValue = 0 en caso de no recibir ningun valor del App.tsx
 interface Props {
     initialValue?: number
 }
 
-export const Counter = ( { initialValue = 0 }: Props) => {
+export const CounterReducerComponent = ( { initialValue = 0 }: Props) => {
 
     const [counter, setcounter] = useState(initialValue)
 
